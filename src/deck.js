3edfaf357e9b1dfaecc79a465a5d510681f34118
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-01 22:32:56 -0500

    basic game structure in place. Still getting some errors

--- src/deck.js
+++ src/deck.js
@@ -1,86 +1,39 @@
-// deck.js file must export a single function which
-//
-//     returns an array of shuffled card values.
-//     There should be FOUR of each value: 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', and 'A'.
-//     Note that you DO NOT need to worry about suits (unless you do Adventure Mode).
-//
-//
-// 1) create a function that spits pout a random number between 1 and 52
-//
-//
-//
-//
 
 
 
- // module.exports =
- function buildDeck(maxNumber){
 
-  let deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];//array of cards
-  let draw;//rng
-  let dealArray = [];
-  let countObj = {};
+module.exports = function buildDeck() {
 
-  //I need to create a loop to add more values to the array
-  for(i=0; i<=52; i++){//loop
-    draw = (Math.ceil(Math.random() * maxNumber) - 1);//random number
-    dealArray.push(deck[draw]);
-        // countObj.draw = countObj[draw] + 1;
+
+
+
+   let shuffleAr = [];
+   let card = 1;
+      for (j=0;j<13;j++){
+          card ++;
+        for(i=0; i <4; i++){
+            shuffleAr.push(card);
+
+          // if (card === 11){
+          //   shuffleAr.push('J');
+          //     }else if (card === 12){
+          //       shuffleAr.push('Q');
+          //     }else if (card === 13) {
+          //       shuffleAr.push('K');
+          //     }else if (card === 14){
+          //       shuffleAr.push('A');
+          //     }else {
+          //       shuffleAr.push(card);
+          //     }
           //
-          // draw = (Math.ceil(Math.random() * maxNumber) - 1);//this might not be needed
-          // i = i - 1;
-      }
-      // console.log(dealArray);
-      return dealArray;
-
+           }
+          }
+          // console.log(shuffleAr);//check for the array
+          // console.log(shuffleAr.length); //check that the right amount of cards have been dealt
+          return (shuffleAr);
 };
 
 
-// buildDeck(13);
-let output = buildDeck(13);
-console.log(output);
 
-
-
-
-
-
-//must return an array of shuffled card values
-//
-//There should be FOUR of each value:
-// 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', and 'A'.
-//
-//
-// I need to put a count on each card. If the count reaches 4, then the dealer
-// function must return a suite value that doesnt equal 4. It will deal these cards
-// unitl all cards have reach the value of 4
-//
-// 10-13 will be face cards
-//
-//
-// first- i need to build my deck with 4 of each value
-//    todo this is should
-//        run my rng
-//        check if that value has already been generated 4 times
-//            if it hasnt-push it into and array
-//            otherwise -re roll and try again.
-//            when the generator has reached 52 cards total (or 26 in two arrays)
-//            stop rolling
-//
-//
-//
-//
-//            function wordFrequency(inputString){
-//     let splitString = inputString.split(' ');
-//     let stringContainer = {};
-//
-//     splitString.forEach(function propertyAdd(each){
-//         if (stringContainer[each] >= 1){
-//             stringContainer[each] = stringContainer[each] + 1;
-//           }else {
-//               stringContainer[each] = 1;
-//               }
-//         });
-//
-//         return stringContainer;
-// //
+// let output = buildDeck();
+// console.log(output);
--- src/game.js
+++ src/game.js
@@ -1,10 +1,84 @@
 
 
-let deck = require('./deck.js');
 
-console.log(deck);
 
-//function playerStuff (playerOne, playerTwo) {
-//
-// I need to deal two card then compare them. if card one has a higher value than
-// card two, player one gets a win, else player two gets a win.
+
+
+let output = require('./deck.js');
+let deck = output();
+
+
+
+function scores (player1, player2){
+
+let pOneWins = 0;
+let pTwoWins = 0;
+let ties = 0;
+let hands = 0;
+let j = i + 1;
+
+
+
+
+console.log(deck.length);
+console.log (deck);
+
+   for(i=1; i<=deck.length; i++){
+
+      if (deck[i] > deck[j]){
+        hands++;
+        pOneWins++;
+        i++;
+        j++;
+
+      } else if (deck[i] < deck[j]){
+
+        hands++;
+        pTwoWins++;
+        i++;
+        j++;
+    }
+
+      else
+        hands++;
+        ties++;
+        i++;
+        j++;
+      }
+
+
+
+
+      let poneobj = {
+          name:'PLayer1',
+          numberOfWins:pOneWins,
+          winRatio:0
+      };
+
+      let ptwoobj = {
+          name:'PLayer2',
+          numberOfWins:pTwoWins,
+          winRatio:0
+      };
+
+
+    let scoreSheet = {
+        date: 0,
+        players: [poneobj , ptwoobj],
+        numberOfTies:ties
+    };
+
+
+
+
+    console.log(scoreSheet);
+    console.log(ptwoobj);
+    console.log(poneobj);
+    // console.log('Player 1 ' + pOneWins);
+    // console.log('Player 2 ' + pTwoWins);
+    // console.log('Ties ' + ties);
+    console.log('Hands played ' + hands);
+
+}
+
+scores();
